97eff2fb1bf9a4bac27d4bc023865b74a7fe70cd
Mohammed Motar <mohammed.motar.mm@gmail.com>
2020-09-25 13:44:07 +0200

    putsade koden :)

--- main.js
+++ main.js
@@ -19,79 +19,9 @@ let runFnOnce = true;
 // save item in this array
 let arr = [];
 
-// Renders items when page reloads, runs once
-const fetchOnce = () => {
+const fetchData = (date) => {
   axios(
-    `http://data.fixer.io/api/${currentDate || "latest"}?access_key=${API_KEY}`
-  ).then((res) => {
-    let rates = res.data.rates;
-
-    for (result in rates) arr.push({ type: result, value: rates[result] });
-
-    // render direct
-    arr.sort((a, b) => a.value - b.value);
-    arr.map((item) => {
-      let tr = document.createElement("tr");
-      let td = document.createElement("td");
-      let td2 = document.createElement("td");
-
-      td.innerHTML = item.type;
-      td2.innerHTML = item.value;
-      tr.appendChild(td);
-      tr.appendChild(td2);
-      tbody.appendChild(tr);
-    });
-
-    // Listens for changes in <select>
-    select.addEventListener("change", (e) => {
-      if (e.target.value === "ascend") {
-        tbody.innerHTML = "";
-
-        arr.sort((a, b) => a.value - b.value);
-        arr.map((item) => {
-          let tr = document.createElement("tr");
-          let td = document.createElement("td");
-          let td2 = document.createElement("td");
-
-          td.innerHTML = item.type;
-          td2.innerHTML = item.value;
-          tr.appendChild(td);
-          tr.appendChild(td2);
-          tbody.appendChild(tr);
-        });
-      } else {
-        tbody.innerHTML = "";
-
-        arr.sort((a, b) => b.value - a.value);
-        arr.map((item) => {
-          console.log(item);
-          let tr = document.createElement("tr");
-          let td = document.createElement("td");
-          let td2 = document.createElement("td");
-
-          td.innerHTML = item.type;
-          td2.innerHTML = item.value;
-          tr.appendChild(td);
-          tr.appendChild(td2);
-          tbody.appendChild(tr);
-        });
-      }
-    });
-  });
-};
-
-// Runs when the date changes
-input.addEventListener("change", (e) => {
-  tbody.innerHTML = "";
-
-  // When i change date in input, then the function stops and this eventListener takes over
-  runFnOnce = false;
-
-  // Making the array empty when changing the date and when going over from the fetchOnce() to this eventListener
-  arr = [];
-
-  axios(
-    `http://data.fixer.io/api/${e.target.value}?access_key=${API_KEY}`
+    `http://data.fixer.io/api/${date || "latest"}?access_key=${API_KEY}`
   ).then((res) => {
     const rates = res.data.rates;
 
@@ -153,6 +83,24 @@ input.addEventListener("change", (e) => {
       }
     });
   });
+};
+
+// Renders items when page reloads, runs once
+const fetchOnce = () => {
+  fetchData(currentDate);
+};
+
+// Runs when the date changes
+input.addEventListener("change", (e) => {
+  tbody.innerHTML = "";
+
+  // When i change date in input, then the function stops and this eventListener takes over
+  runFnOnce = false;
+
+  // Making the array empty when changing the date and when going over from the fetchOnce() to this eventListener
+  arr = [];
+
+  fetchData(e.target.value);
 });
 
 runFnOnce && fetchOnce();
